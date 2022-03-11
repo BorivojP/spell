751b633838e822fbfda0506fc5323120d98f3e6d
BokiPanticB <72427445+BokiPanticB@users.noreply.github.com>
2022-03-11 10:48:58 -0800

    added prog

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,6 +9,60 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    app.UseResponseCompression();
+
+    app.UseRouting();
+    
+    app.UseResponseCaching();
+    
+    env.WebRootFileProvider = new PhysicalFileProvider(
+            Path.Combine(env.ContentRootPath, "UI/build"));
+
+    app.UseDefaultFiles();
+    
+    app.UseStaticFiles(new StaticFileOptions()
+    {
+        HttpsCompression = Microsoft.AspNetCore.Http.Features.HttpsCompressionMode.Compress,
+        OnPrepareResponse = (context) =>
+        {
+            var headers = context.Context.Response.GetTypedHeaders();
+            headers.CacheControl = new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+            {
+                Public = true,
+                MaxAge = TimeSpan.FromDays(1)
+            };
+        }
+    });
+
+    app.UseSpaStaticFiles(new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(
+            Path.Combine(env.ContentRootPath, "UI/build")),
+        RequestPath = "/Static",
+        HttpsCompression = Microsoft.AspNetCore.Http.Features.HttpsCompressionMode.Compress,
+        OnPrepareResponse = (context) =>
+        {
+            var headers = context.Context.Response.GetTypedHeaders();
+            headers.CacheControl = new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+            {
+                Public = true,
+                MaxAge = TimeSpan.FromDays(1)
+            };
+        }
+    });
+    app.UseSpa(spa =>
+    {
+        spa.Options.SourcePath = "UI";
+
+        if (env.IsDevelopment())
+        {
+            spa.UseReactDevelopmentServer(npmScript: "start");
+        }
+    });
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -21,5 +75,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 
 app.Run();
